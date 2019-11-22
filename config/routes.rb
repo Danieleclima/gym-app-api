885847ee65edfc13a3f30a4e8879b45c0b063cf8
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-22 19:49:06 +0000

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :locations, only: [:show, :index, :new, :create, :edit, :update] do
+    resources :gyms, only: [:index, :show]
+  end
+  
   resources :locations
   resources :gyms
+ 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
