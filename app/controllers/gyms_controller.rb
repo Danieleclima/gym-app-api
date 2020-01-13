8efd72856a7bab013bda6a74069e896eba35b0ed
Danieleclima <daniel.echuaca.lima@gmail.com>
2020-01-13 20:49:26 +0000

    last changes

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -9,13 +9,20 @@ class GymsController < ApplicationController
     end
 
     def create
-        l = Location.find_by_city(params[:gym][:city_name])
-        g = Gym.create(name: params[:gym][:name], city_name: params[:gym][:city_name], location_id: l.id)
+        l = Location.find_or_create_by(city: params[:gym][:city_name])
+        g = l.gyms.create(gym_params)
+        # g = l.gyms.create(name: params[:gym][:name], city_name: params[:gym][:city_name], location_id: l.id, image: params[:gym][:image], url: params[:gym][:url])
         # render json: g
-        # binding.pry
+        binding.pry
     end
 
     def show
        
+    end
+
+    private
+
+    def gym_params
+         params.require(:gym).permit(:name, :city_name, :location_id, :image, :url)
     end
 end
